c6881055e9b6aef01578d4fdccc0bca3c6e2097c
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-09 01:18:25 +0200

    onPlay = function

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+const player = new Player('vimeo-player');
 
+const onPlay = function (data) {
+  const currentTime = data.seconds;
+  
+  console.log(currentTime);
+};
+
+player.on('play', onPlay);
