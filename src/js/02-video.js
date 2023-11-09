9cfc85069cd87d57b9f1f249f4b5d0a9d7b78ede
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-09 02:14:46 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,26 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 const player = new Player('vimeo-player');
 
-const onPlay = function (data) {
+const onPlay = throttle(function (data) {
   const currentTime = data.seconds;
-//   console.log(currentTime);
-  localStorage.setItem("videoplayer-current-time", currentTime);
-
-
-
-};
+  //   console.log(currentTime);
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
 
 player.on('timeupdate', onPlay);
-const savedTime = localStorage.getItem("videoplayer-current-time");
+const savedTime = localStorage.getItem('videoplayer-current-time');
 console.log(savedTime);
 
-player.setCurrentTime(savedTime).then(function(seconds) {
-
-   
-}).catch(function(error) {
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
     switch (error.name) {
-        case 'RangeError':
-           
-            break;
+      case 'RangeError':
+        break;
 
-        default:
-           
-            break;
+      default:
+        break;
     }
-});
+  });
