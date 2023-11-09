ee73a72261c7344854247b702d0a421394f74a0d
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-09 02:06:29 +0200

    setCurrentTime()

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,28 @@ const player = new Player('vimeo-player');
 
 const onPlay = function (data) {
   const currentTime = data.seconds;
-  console.log(currentTime);
+//   console.log(currentTime);
   localStorage.setItem("videoplayer-current-time", currentTime);
+
 
 
 };
 
 player.on('timeupdate', onPlay);
+const savedTime = localStorage.getItem("videoplayer-current-time");
+console.log(savedTime);
 
+player.setCurrentTime(savedTime).then(function(seconds) {
 
+   
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+           
+            break;
 
-
-
+        default:
+           
+            break;
+    }
+});
